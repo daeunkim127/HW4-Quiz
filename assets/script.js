394463a558588561f2a64d47300cae8528aba42a
Da Kim <daeunkim127@gmail.com>
2022-03-29 03:55:14 -0400

    score was going up by 2. fixed

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ var choice2=document.getElementById('choice2');
 var choice3=document.getElementById('choice3')
 var startBtn=document.getElementById('startBtn')
 var timeLeft=document.getElementById('timer')
+var nextBtn = document.getElementById('nextBtn')
 
 
 
@@ -66,14 +67,14 @@ var questionList = [
 var scores =0;
 var i=0;
 var q = questionList[i];
-var questionTime=10;
+var questionTime=30;
 
 function quizGenerator() {
+    console.log(scores)
     currentQuestion.innerHTML="<p>"+ q.question+"</p>";
     choice1.innerHTML= q.a;
     choice2.innerHTML= q.b;
     choice3.innerHTML= q.c;
-
 };
 
 function correct() {
@@ -88,7 +89,6 @@ function incorrect() {
     submitBtn.style.visibility='hidden';
     result.innerHTML="<p> Wrong! </p>";
     result.classList.add('correct')
-    scores--;
 
 }
 
@@ -99,8 +99,11 @@ startBtn.addEventListener('click', function() {
     quizGenerator();
     quizContent.style.visibility='visible'
     var timer=setInterval(function(){
-        if(questionTime<=0){
+        if(questionTime<0){
             clearInterval(timer);
+            quizContent.style.display='none'
+            var initial=window.prompt("Time's up! Your Score is"+scores+"/7. Enter Your Initial.")
+            timeLeft.style.display='none'
         } else {
        timeLeft.innerHTML=questionTime+' seconds remaining';}
        questionTime -=1
@@ -112,8 +115,29 @@ submitBtn.addEventListener('click', function(event){
     event.preventDefault();
     var checked = document.querySelector('input[name=choice]:checked');
     if ( q.correctAnswer==checked.value) {
-        correct()
+        correct();
+        nextBtn.style.visibility='visible';
     } else {
+        questionTime -= 5;
         incorrect()
+        nextBtn.style.visibility='visible'
     }
+})
+
+nextBtn.addEventListener('click',function(event){
+    event.preventDefault();
+    i++;
+    q=questionList[i];
+    nextBtn.style.visibility='hidden';
+    submitBtn.style.visibility='visible';
+    result.textContent='';
+
+    if(i>=7) {
+        clearInterval(timer);
+        var initial= window.prompt('Finished! Your Score is '+scores+'/7. Enter Your Initial.');
+        timeLeft.style.display='none'
+    } else {
+        quizGenerator()
+    }
+
 })
